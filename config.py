57c44860f5716cb8265d6760fc31ee1dec2d0b70
ChenGuangHai <954182252@qq.com>
2018-03-31 17:19:57 +0800

    项目立项，环境框架基本搭建。

---
+++ config.py
@@ -0,0 +1,28 @@
+# --*-- coding:utf-8 --*--
+from redis import StrictRedis
+
+class Config(object):
+    """应用程序配置类"""
+    #开启调试模式
+    DEBUT=True
+
+    #配置secret key,简单生成方法，ipthon 中 base64.b64encode(os.urandom(48))
+    SECRET_KEY='ix4En7l1Hau10aPq8kv8tuzcVl1s2Zo6eA+5+R+CXor8G3Jo0IJvcj001jz3XuXl'
+
+    #orm连接数据库
+    SQLALCHEMY_DATABASE_URI='mysql://root:root@127.0.0.1:3306/ihome'
+    #是否开启追踪
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #配置Redis数据库
+    REDIS_HOST='127.0.0.1'
+    REDIS_PORT = 6379
+
+    # 配置session数据存储到redis数据库
+    SESSION_TYPE = 'redis'
+    # 指定存储session数据的redis的位置
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT,db=1)
+    # 开启session数据的签名，意思是让session数据不以明文形式存储
+    SESSION_USE_SIGNER = True
+    # 設置session的会话的超时时长 ：一天
+    PERMANENT_SESSION_LIFETIME = 3600 * 24
---
+++ manage.py
@@ -0,0 +1,32 @@
+# --*-- coding:utf-8 --*--
+from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
+from flask_session import Session
+
+#创建app
+app=Flask(__name__)
+#加载配置文件
+app.config.from_object(Config)
+#创建数据库连接对象
+db=SQLAlchemy(app)
+#让迁移时，app和数据库建立关联
+Migrate(app,db)
+#创建脚本管理器
+manager=Manager(app)
+#增加db脚本命令
+manager.add_command('db',MigrateCommand)
+#session绑定app
+Session(app)
+
+#测试视图
+@app.route('/',methods=['GET','POST'])
+def index():
+    # redis数据库存储session测试
+    session['name']='hahaha'
+    return 'index'
+
+if __name__ == '__main__':
+    app.run()
