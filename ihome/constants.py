43fbaf7ca9eb9c8cda7511dea336b7cf95cbf40f
ChenGuangHai <954182252@qq.com>
2018-04-01 21:55:43 +0800

    导入生成图片验证码框架，魔法数字文件，响应状态码文件

---
+++ ihome/constants.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+# 图片验证码Redis有效期， 单位：秒
+IMAGE_CODE_REDIS_EXPIRES = 300
+
+# 短信验证码Redis有效期，单位：秒
+SMS_CODE_REDIS_EXPIRES = 300
+
+# 七牛空间域名
+QINIU_DOMIN_PREFIX = "http://o91qujnqh.bkt.clouddn.com/"
+
+# 城区信息redis缓存时间，单位：秒
+AREA_INFO_REDIS_EXPIRES = 7200
+
+# 首页展示最多的房屋数量
+HOME_PAGE_MAX_HOUSES = 5
+
+# 首页房屋数据的Redis缓存时间，单位：秒
+HOME_PAGE_DATA_REDIS_EXPIRES = 7200
+
+# 房屋详情页展示的评论最大数
+HOUSE_DETAIL_COMMENT_DISPLAY_COUNTS = 30
+
+# 房屋详情页面数据Redis缓存时间，单位：秒
+HOUSE_DETAIL_REDIS_EXPIRE_SECOND = 7200
+
+# 房屋列表页面每页显示条目数
+HOUSE_LIST_PAGE_CAPACITY = 2
+
+# 房屋列表页面Redis缓存时间，单位：秒
+HOUSE_LIST_REDIS_EXPIRES = 7200
